426ab3c53d6b7cfe7ca6c6c91ec6f1e1ff1ab909
Tanner <tannn3r@gmail.com>
2014-05-30 16:51:57 -0500

    Upload files

---
+++ index_style/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"1201 http:\/\/trivialand.org\/?p=1201","comments":0}]});
+}
